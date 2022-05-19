2ea2ae10aafd0e5eb0bfaeebe952a66aa5f478d5
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-05-19 15:49:46 +0100

    set up for feed view (multiple nfts)

--- src/scene.js
+++ src/scene.js
@@ -3,14 +3,15 @@ import { D3DNFTViewer } from '3d-nft-viewer'
 
 export const createScene = (el) => {
 //initialize NFT viewer front end
+console.log('createScene');
   let nftViewer = new D3DNFTViewer({
     defaultLoader: 'gltf',
-    ctrClass: 'nft-viewer', // Attribute of div containing image
+    ctrClass: 'nft-post', // Attribute of div containing post and any additionl html
     nftDataAttr: 'data-nft', // Attribute of div.ctrClass containing post has hex
-    nftsRoute: 'nfts', // Back end route to initialize NFTs
-    modelsRoute: 'models', // Back end route to load models
+    nftsRoute: 'http://localhost:3000/nfts', // Back end route to initialize NFTs
+    modelsRoute: 'http://localhost:3000/models', // Back end route to load models
     linkText: 'View in 3D',
-    previewCtrCls: 'post-wrapper', //container element in which to create the preview
+    previewCtrCls: 'container', //container element in which to create the preview
     skyboxes: true
   });
   nftViewer.initNFTs();
