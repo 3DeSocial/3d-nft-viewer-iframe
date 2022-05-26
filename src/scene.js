697b7a0cdb609fc4902b30c562b910e39d2b7b87
Andrew van Duivenbode <andrew@creativetransmissions.com>
2022-05-26 13:37:56 +0100

    update path format

--- src/scene.js
+++ src/scene.js
@@ -8,13 +8,13 @@ export const createScene = (el) => {
     defaultLoader: 'gltf',
     ctrClass: 'nft-post', // Attribute of div containing post and any additionl html
     nftDataAttr: 'data-nft', // Attribute of div.ctrClass containing post has hex
-    nftsRoute: 'http://localhost:3000/nfts/', // Back end route to initialize NFTs
+    nftsRoute: 'http://localhost:3000/nfts', // Back end route to initialize NFTs
     modelsRoute: 'http://localhost:3000/models', // Back end route to load models
     linkText: 'View in 3D',
     linkCtrCls: 'nft-viewer', // container for links such as view in 3d, view in vr
     previewCtrCls: 'container', //container element in which to create the preview
     skyboxes: true,
-    skyboxPath: 'http://localhost:3000/images/skyboxes/',
+    skyboxPath: 'http://localhost:3000/images/skyboxes',
     sceneryPath: 'http://localhost:3000/layouts/round_showroom/scene.gltf',
     useShowroom: true,
     scaleModelToHeight: 5,
